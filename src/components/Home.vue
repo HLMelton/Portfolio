97feea086508fab709cdebe14c14cda66572df1c
Hunter Melton <hunterlmelton@gmail.com>
2022-07-12 01:40:33 -0700

    Added Fonts and Alternate Pages

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,8 +12,10 @@ export default {
 <template>
     <div className="flex flex-row w-full absolute z-64 p-16">
         <div className="flex flex-col">
-            <h1 className=" text-white font-mono text-xl"> Hunter L Melton </h1>
-        <p className="text-white">I'm an Independent Developer </p>
+            <h1 className=" text-white font-FuturaM text-8xl"> Hunter </h1>
+            <h1 className=" text-white font-FuturaM text-8xl"> Melton </h1>
+            <p className="text-white text-3xl font-FuturaM"> Web Developer /</p>
+            <p className="text-white text-3xl font-FuturaM"> Consultant </p>
         </div>
         <!-- Time for ThreeJS integration -->
         <Keystone />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+      fontFamily: {
+        // 'Koulen' : 'Koulen',
+        'FuturaM' : 'FuturaM'
+      }
+    }
+      
+    },
   plugins: [],
-}
+  }
